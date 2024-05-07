45528fdf86f8cdfcc44a25b8eea08f357c99d26b
K Harsh <harshnaidu2u@gmail.com>
2024-05-07 15:22:41 +0530

    db connection, pre hook, hash password

--- backend/controllers/userConrtroller.js
+++ backend/controllers/userConrtroller.js
@@ -1,4 +1,5 @@
 import expressAsyncHandler from "express-async-handler";
+import User from "../models/users.model.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
@@ -16,10 +17,31 @@ const authUser = expressAsyncHandler(async (req, res, next) => {
 // @access  Public
 
 const signupUser = expressAsyncHandler(async (req, res, next) => {
-    res.status(200).json({
-        status: "success",
-        message: "signup"
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email })
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists.')
+    }
+
+    const user = await User.create({
+        name,
+        email, 
+        password
     })
+
+    if(user){
+        res.status(201).json({
+            status: "success",
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else{
+        res.status(400);
+        throw new Error('Invalid user data.')
+    }
 })
 
 // @desc    logout user
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
+import connectDB from "./config/db.js";
 
 import usersRoutes from "./routes/userRoutes.js"
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 
+connectDB()
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 5000
 
 app.use('/api/users', usersRoutes)
